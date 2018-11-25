00bcbe48f3161ca63bb0d636aad80aded163b395
William Zerbato <william.zerbato@esme.fr>
2018-11-25 15:21:24 -0500

    commit for ligntning tests

---
+++ TheSecretLivesOfToys/Assets/Destruction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruction : MonoBehaviour {
+
+    public GameObject DestroyedGO;
+    public GameObject explosionEffect;
+
+    public float radius;
+    public float force;
+
+    private void OnDestroy()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Instantiate(DestroyedGO, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearObject in colliders)
+        {
+            Rigidbody rb = nearObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+
+        }
+    }
+}
---
+++ TheSecretLivesOfToys/Assets/DestructionDelay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructionDelay : MonoBehaviour {
+
+    public float delay = 3f;
+
+    float countdown;
+    bool hasExploded = false;
+
+    private void Start()
+    {
+        countdown = delay;
+    }
+
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f && !hasExploded)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
